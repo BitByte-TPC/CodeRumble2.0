edbf8fe6566614655822f6f6cfffb0646e7c375c
SamakshAgarwal1112 <agarwalsamaksh11@gmail.com>
2024-08-12 22:32:30 +0530

    fix: minor UI fixes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import {Navbar, Footer} from '../components';
 import {Hero, About, Perks, Tle31} from '../sections';
 const Page =()=>(
-  <div className="mx-6">
+  <div className="mx-6 overflow-x-hidden">
     <Navbar/>
     <Hero/>
     <About/>
--- components/PerksCard.jsx
+++ components/PerksCard.jsx
@@ -1,6 +1,6 @@
 export default function PerksCard({head, text}) {
     return(
-        <div className="flex flex-col gap-10 bg-[#FFFFFF1A] p-7 md:p-10 rounded-3xl md:rounded-[2.5rem] pr-5 md:pr-32 md:h-80 w-48 md:w-[33.5rem] my-5 md:mb-0">
+        <div className="flex flex-col gap-10 bg-[#FFFFFF1A] p-7 md:p-10 rounded-3xl md:rounded-[2.5rem] pr-5 md:pr-32 md:h-80 w-48 md:w-[100%] md:min-w-[33.5rem] my-5 md:mb-0">
                 <h1 className="text-xs md:text-xl font-bold tracking-[0.4rem] md:tracking-[1rem] text-[#FFFFFF80] md:py-2">{head}</h1>
                 <p className=" text-base md:text-[1.7rem] font-bold leading-relaxed">{text}</p>
         </div>
