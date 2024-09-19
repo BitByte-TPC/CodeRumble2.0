cad8ffea93bfa205018a53a4bca627168539c11d
NightFury742 <eshaan122@gmail.com>
2024-09-19 21:35:17 +0530

    changed register button

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -4,32 +4,34 @@ import { HeroFooter, Lumi, Navbar } from "../components";
 const Hero = () => (
   <>
     <Navbar />
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-grow flex flex-col items-center justify-center ">
+    <div className="flex flex-col min-h-[100svh]">
+      <div className="flex-grow flex flex-col items-center justify-center">
         <div
           style={{
             backgroundImage: "url(./heroLogo.webp)",
-            backgroundSize: "contain", // 'contain' will scale the image to fit within the container without cropping
+            backgroundSize: "contain",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
-            height: "100%", // Adjust this to cover more area
-            width: "100%", // Adjust this to cover more area
+            height: "100%",
+            width: "100%",
           }}
         >
-          <div className="flex flex-col items-center gap-6 ">
+          <div className="flex flex-col items-center gap-6">
             <Lumi />
           </div>
         </div>
         <button
           onClick={() =>
-            (window.location.href = "https://code-rumble.devfolio.co/overview")
+            (window.location.href = " https://discord.gg/8Qu8dsYfCS")
           }
           className="md:hidden bg-[#1A1A1A] px-8 py-3 rounded-full text-white font-bold hover:bg-[#282828] hover:scale-105 transition-transform duration-300"
         >
-          Register Now
+         Join Discord!
         </button>
       </div>
-      <HeroFooter />
+      <div className="mt-auto mb-4">
+        <HeroFooter />
+      </div>
     </div>
   </>
 );
